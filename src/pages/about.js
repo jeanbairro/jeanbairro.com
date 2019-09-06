2ce152b726bf646268f5dcc01bb000e6488c0a0c
Jean Bairro <jeancasulo@gmail.com>
2019-09-05 21:46:16 -0300

    criando o projeto

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+    <div style={{ color: `purple` }}>
+        <Header headerText="About Gatsby" />
+        <h1>Jean Bairro</h1>
+        <p>Engenheiro de software, boleiro nas horas vagas.</p>
+    </div>
+)
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+    <div style={{ color: `purple` }}>
+        <Header headerText="Index Gatsby" />
+        <h1>Olá Nhiblo!</h1>
+        <p>Que passa hombre?</p>
+    </div>
+)
