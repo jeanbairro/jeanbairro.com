99f2d9eec753066a259dc20a2a8906ff92ef101f
Jean Bairro <jeancasulo@gmail.com>
2020-06-11 11:55:50 -0300

    Melhorando a responsividade do menu

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -5,9 +5,8 @@
         <g-link to="/">jeanbairro.com</g-link>
       </strong>
       <nav class="nav">
-        <g-link class="nav__link" to="/sobre">Sobre</g-link>
-        <a href="https://github.com/jeancasulo" target="_blank">GitHub</a>
-        <a href="https://www.linkedin.com/in/jean-bairro" target="_blank">Linkedin</a>
+        <a href="https://github.com/jeancasulo" target="_blank"><i class="fa fa-github"></i></a>
+        <a href="https://www.linkedin.com/in/jean-bairro" target="_blank"><i class="fa fa-linkedin"></i></a>
       </nav>
     </header>
     <div class="container">
@@ -25,6 +24,8 @@ query {
 </static-query>
 
 <style lang="less">
+@smallAndUp: ~"only screen and (min-width: 576px)";
+
 .layout {
   font-size: 1.2rem;
   margin: 0 auto;
@@ -33,19 +34,36 @@ query {
     align-items: center;
     background-color: black;
     display: flex;
-    height: 65px;
+    height: 50px;
     justify-content: space-between;
     margin-bottom: 2.4rem;
-    padding: 0 5rem;
+    padding: 0 1.2rem;
 
     a {
-      color: #f1f1f1;
+      color: #f1f1f1;;
       margin-right: 1rem;
+
+      .fa {
+        font-size: 1.4rem
+      }
 
       &:hover {
         color: #fff;
         text-decoration: none;
       }
+
+      &:last-child {
+        margin: 0;
+      }
+    }
+  }
+}
+
+@media @smallAndUp {
+  .layout {
+    .header {
+      height: 65px;
+      padding: 0 10rem;
     }
   }
 }
--- src/pages/Sobre.vue
+++ src/pages/Sobre.vue
@@ -2,6 +2,7 @@
   <Layout>
     <h1>Sobre</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <g-link class="nav__link" to="/sobre">Sobre</g-link>
   </Layout>
 </template>
 
