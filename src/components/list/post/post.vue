0222062de77f881ab0d54626dd205b8a2c92ad54
Jean Bairro <jeancasulo@gmail.com>
2020-06-10 22:01:38 -0300

    Criação do componente post

---
+++ src/components/list/post/post.vue
@@ -0,0 +1,13 @@
+<template src="./post.html"></template>
+<style lang="less" link="./post.less"></style>
+
+<script>
+export default {
+    props: {
+        subtitle: String,
+        title: String,
+        time: String,
+        tags: Array
+    }
+}
+</script>
